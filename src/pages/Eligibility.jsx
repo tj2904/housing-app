910201ffef32caeb4ad69be2b37610f70d8063fc
tj2904 <tj2904+github@gmail.com>
2022-10-27 14:17:18 +0100

    adds palceholder images

--- src/pages/Eligibility.jsx
+++ src/pages/Eligibility.jsx
@@ -12,9 +12,46 @@ function Eligibility() {
     <>
       <h2>Legal Aid Eligibility Questionnaire</h2>
 
-{/* Add the redirect to the gov.uk form here - maybe an iframe? */}
+      {/* Add the redirect to the gov.uk form here - maybe an iframe? */}
 
-      <div >
+      <h3 className="p-4">
+        Please check if you qualify for Legal Aid using the government site:
+      </h3>
+
+      <p>
+        Examples of images they will see, ie. both outcomes and the page the
+        should see first
+      </p>
+      <img
+        src="https://via.placeholder.com/300x200?text=start_screen"
+        className="fluid m-2"
+      />
+      <img
+        src="https://via.placeholder.com/300x200?text=You_Are_Not_Eligible"
+        className="fluid m-2"
+      />
+      <img
+        src="https://via.placeholder.com/300x200?text=You_Are_Eligible_screen"
+        className="fluid m-2"
+      />
+      {/* 
+      This approach doesn't work
+      
+      <iframe
+        src="https://www.gov.uk/check-legal-aid"
+        width={1000}
+        height={500}
+        sandbox="allow-scripts allow-modal allow-same-origin"
+      ></iframe> */}
+
+      <div>
+        <a href="https://www.gov.uk/check-legal-aid"
+          role="button"
+          className="m-2 btn btn-dark"
+          target="_blank"
+        >
+          Check if you are eligible for Legal Aid
+        </a>
         <Link to="/details" role="button" className="m-2 btn btn-secondary">
           See Details of Solicitors
         </Link>
