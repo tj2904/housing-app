9ed7d12bdafef8b47a2192a9420ed64ff39eda4a
tj2904 <tj2904+github@gmail.com>
2022-11-04 13:08:15 +0000

    refactors solicitor details card into a component

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,62 +1,33 @@
 import React from "react";
-import { Card } from "react-bootstrap";
-import CardHeader from "react-bootstrap/esm/CardHeader";
 import { Link } from "react-router-dom";
-import data from "../data/housing-solicitors"
+import SolicitorCard from "../components/SolicitorCard";
+import data from "../data/housing-solicitors";
 
 export default function Details() {
-  
-  const firm = data;
+  // Here we map through all of the firms in the list
+  // returning a SolicitorCard component for each one (singleFirm)
+  const firm = data.map((singleFirm) => {
+    return <SolicitorCard {...singleFirm} />;
+  });
 
   return (
-    // This uses react bootstrap components to build the card.
-    // It will need altering to function well in the final app so that it can map over results
-    // We need to decide how this is powered at somepoint too - JSON maybe the best way forward,
-    // but we could use a db
-
-    // If keeping maps this needs to be implemented properly
     <>
       <h2>Law Firm Details Page</h2>
+      <h5 className="my-4 lead">
+        The organisations listed below are known to offer Legal Aid services for
+        your kind of case.
+      </h5>
+      <p>
+        Use the contact details below to try to arrange an appointment and/or
+        for them to agree to represent you and your case at court.
+        <br />
+        When you have either found representation, or exhausted the list of
+        contacts click the button at the bottom of the page to complete our
+        pre-court form to support you and your legal counsel to best represent
+        your case in court.
+      </p>
       <div className="pt-2">
-        <div className="row g-0">
-          {firm.map((firm) => {
-
-            return (
-              <Card key={firm.id} className=" mb-4 col-12 col-lg-8 mx-auto">
-                <CardHeader>
-                  <Card.Title>{firm.Firm}</Card.Title>
-                </CardHeader>
-                <div className="row g-0">
-                  <div className="col-md-6">
-                    <Card.Body>
-                      <address>
-                        {firm.StreetName} <br />
-                        {firm.City} <br />
-                        {firm.Postcode} <br />
-                      </address>
-                      ☎ <a href={`tel:${firm.Telephone}`}>{firm.Telephone}</a>
-                      <br />
-                      📧 <a href={`mailto:${firm.email}`}>{firm.email}</a>{" "}
-                      <br />
-                      🖥  <a href={firm.website} target="_blank" rel="noreferrer">
-                        {firm.website}
-                      </a>
-                    </Card.Body>
-                  </div>
-                  <iframe
-                    className="col-md-6"
-                    src={firm.mapEmbed}
-                    allowFullScreen=""
-                    loading="lazy"
-                    title="Firm1"
-                    referrerPolicy="no-referrer-when-downgrade"
-                  ></iframe>
-                </div>
-              </Card>
-            );
-          })}
-        </div>
-        {/* End of row */}
+        {firm}
 
         <div className="text-center">
           <Link to="/court" role="button" className="m-2 btn btn-info ">
