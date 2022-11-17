b4a7eb5d30ba483d137ee49a7b0d80036844a678
lmarchiorit <98050866+lmarchiorit@users.noreply.github.com>
2022-11-17 14:38:01 +0000

    Update CourtInfo.jsx
    I've added the questions, I opted for the short answer fields because it doesn't seem to have any word limit anyway.
    
    Please let me know if any changes are necessary.

--- src/pages/CourtInfo.jsx
+++ src/pages/CourtInfo.jsx
@@ -16,37 +16,96 @@ function CourtInfo() {
       </div>
 
       <Form>
-        <Form.Group className="mb-3" controlId="question1">
-          <Form.Label>Queston 1</Form.Label>
+        <Form.Group className="mb-3" controlId="Name:" required>
+          <Form.Label>Question 1<span className="text-danger fs-4">*</span></Form.Label>
           <Form.Control type="text" placeholder="Short answer" />
           <Form.Text className="text-muted">
             Add additional instructions if needed here.
           </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="question2" required>
+        <Form.Group className="mb-3" controlId="Address:" required>
           <Form.Label>Question 2 <span className="text-danger fs-4">*</span></Form.Label>
           <Form.Control type="text" placeholder="Another short answer" />
         </Form.Group>
-
-        <Form.Group className="col-sm-6 mb-3">
-          <Form.Label for="question3">
-            Pre-defined drop-down list queston
-          </Form.Label>
-          <Form.Select className="form-select" id="question3">
-            <option selected>Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </Form.Select>
+    
+     <Form.Group className="mb-3" controlId="Email Address:" required>
+          <Form.Label>Question 3 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+    <Form.Group className="mb-3" controlId="Mobile Number:" required>
+          <Form.Label>Question 4 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+    <Form.Group className="mb-3" controlId="Do you have any dependants?" required>
+          <Form.Label>Question 5 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+     <Form.Group className="mb-3" controlId="Do you or the people you live with have any significant physical or mental health problems?" required>
+          <Form.Label>Question 6 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+              <Form.Text className="text-muted">
+            Do you have any disability?
+          </Form.Text>
+        </Form.Group>
+    
+    <Form.Group className="mb-3" controlId="Who is your landlord?">
+          <Form.Label>Question 7 </Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+              <Form.Text className="text-muted">
+            Council, Private landord, other...
+          </Form.Text>
+        </Form.Group>
+    
+    
+    <Form.Group className="mb-3" controlId="What is your income per month?" required>
+          <Form.Label>Question 8 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+    <Form.Group className="mb-3" controlId="Do you receive any benefit?" required>
+          <Form.Label>Question 9 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+    <Form.Group className="mb-3" controlId="How much rent do you owe?">
+          <Form.Label>Question 10 </Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+      <Form.Group className="mb-3" controlId="How much rent do they claim you owe?">
+          <Form.Label>Question 11 </Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+      <Form.Group className="mb-3" controlId="Do you have any other debts?" required>
+          <Form.Label>Question 12 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+      <Form.Group className="mb-3" controlId="What is your plan to clear out your debt?" required>
+          <Form.Label>Question 13 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+      <Form.Group className="mb-3" controlId="Is there any disrepair to your home?" required>
+          <Form.Label>Question 14 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+        </Form.Group>
+    
+      <Form.Group className="mb-3" controlId="Do you have any deadlines?" required>
+          <Form.Label>Question 15 <span className="text-danger fs-4">*</span></Form.Label>
+          <Form.Control type="text" placeholder="Another short answer" />
+              <Form.Text className="text-muted">
+            Court Dates, response to be given to Court/Council.
+          </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="question4">
-          <Form.Label>Question 4</Form.Label>
-          <textarea
-            className="form-control"
-            placeholder="Longer form answer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          />
+
+
         </Form.Group>
         <Button className="disabled mt-4">🖨 Print for Court</Button>
       </Form>
