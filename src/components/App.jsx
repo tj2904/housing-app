b9a5174241a2ba791d6830e36d5a864b4495dafb
tj2904 <tj2904+github@gmail.com>
2022-11-18 19:30:16 +0000

    uses html2pdf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "../components/App.css";
 import Home from "../pages/Home";
 import CourtInfo from "../pages/CourtInfo";
+import CourtInfoPageBreak from "../pages/CourtInfoPageBreak"
 import Details from "../pages/Details";
 import Eligibility from "../pages/Eligibility";
 import { Navigation } from "./Nav/Navigation";
@@ -10,22 +11,20 @@ import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    
-    
     <Router>
-            {/* Adding nav here means it will show on all pages */}
-            <Navigation></Navigation>
-            <Container className="pt-2">
-            <Routes>
-              <Route path="/court" element={<CourtInfo />} />
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/details" element={<Details />} />
-              <Route path="/eligibility" element={<Eligibility />} />
-            </Routes>
-    </Container>
-          </Router>
-
+      {/* Adding nav here means it will show on all pages */}
+      <Navigation></Navigation>
+      <Container className="pt-2">
+        <Routes>
+          <Route path="/court" element={<CourtInfo />} />
+          <Route path="/courtTest" element={<CourtInfoPageBreak />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/eligibility" element={<Eligibility />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/pages/CourtInfoPageBreak.jsx
@@ -0,0 +1,239 @@
+import React, { useRef } from "react";
+import html2pdf from "html2pdf.js"
+import { Link } from "react-router-dom";
+import { Button, Form } from "react-bootstrap";
+
+function CourtInfo() {
+  // pdfDiv is a containing div - everything in it will be in the PDF
+  const pdfDiv = useRef();
+
+  // Handle the click event on the download button
+  const handleDownload = async () => {
+    const element = pdfDiv.current;
+
+    // https://github.com/eKoopmans/html2pdf.js#usage
+    const opt = {
+      margin: 10,
+      filename: "TenantEvictionSupport.pdf",
+    };
+   // html2pdf(element, opt);
+
+   html2pdf().set(opt).from(element).save();
+  };
+
+  return (
+    <>
+      {/*add HTML code to this block before the final </> tag  */}
+      <h2>Court Info Questionnaire</h2>
+      <div className="lead mb-3">
+        Please complete this form ahead of your court date ready to share with
+        your solicitor or the court, so they have all of the information they
+        need to best support you.
+      </div>
+
+      <div ref={pdfDiv}>
+        <Form>
+          <Form.Group className="mb-3 mt-2" controlId="question1">
+            <Form.Label>Queston 1</Form.Label>
+            <Form.Control type="text" placeholder="Short answer" />
+            <Form.Text className="text-muted">
+              Add additional instructions if needed here.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question2" required>
+            <Form.Label>
+              Question 2 <span className="text-danger fs-4">*</span>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Another short answer" />
+          </Form.Group>
+          <Form.Group className="col-sm-6 mb-3">
+            <Form.Label for="question3">
+              Pre-defined drop-down list queston
+            </Form.Label>
+            <Form.Select className="form-select" id="question3">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question4">
+            <Form.Label>Question 4</Form.Label>
+            <textarea
+              className="form-control"
+              placeholder="Longer form answer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            />
+          </Form.Group>
+          {/* <Button className="disabled mt-4">🖨 Print for Court</Button> */}
+          <Form.Group className="mb-3 mt-2" controlId="question1">
+            <Form.Label>Queston 1</Form.Label>
+            <Form.Control type="text" placeholder="Short answer" />
+            <Form.Text className="text-muted">
+              Add additional instructions if needed here.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question2" required>
+            <Form.Label>
+              Question 2 <span className="text-danger fs-4">*</span>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Another short answer" />
+          </Form.Group>
+          <Form.Group className="col-sm-6 mb-3">
+            <Form.Label for="question3">
+              Pre-defined drop-down list queston
+            </Form.Label>
+            <Form.Select className="form-select" id="question3">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question4">
+            <Form.Label>Question 4</Form.Label>
+            <textarea
+              className="form-control"
+              placeholder="Longer form answer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            />
+          </Form.Group>{" "}
+          <Form.Group className="mb-3 mt-2" controlId="question1">
+            <Form.Label>Queston 1</Form.Label>
+            <Form.Control type="text" placeholder="Short answer" />
+            <Form.Text className="text-muted">
+              Add additional instructions if needed here.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question2" required>
+            <Form.Label>
+              Question 2 <span className="text-danger fs-4">*</span>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Another short answer" />
+          </Form.Group>
+          <Form.Group className="col-sm-6 mb-3">
+            <Form.Label for="question3">
+              Pre-defined drop-down list queston
+            </Form.Label>
+            <Form.Select className="form-select" id="question3">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question4">
+            <Form.Label>Question 4</Form.Label>
+            <textarea
+              className="form-control"
+              placeholder="Longer form answer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            />
+          </Form.Group>{" "}
+          <Form.Group className="mb-3 mt-2" controlId="question1">
+            <Form.Label>Queston 1</Form.Label>
+            <Form.Control type="text" placeholder="Short answer" />
+            <Form.Text className="text-muted">
+              Add additional instructions if needed here.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question2" required>
+            <Form.Label>
+              Question 2 <span className="text-danger fs-4">*</span>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Another short answer" />
+          </Form.Group>
+          <Form.Group className="col-sm-6 mb-3">
+            <Form.Label for="question3">
+              Pre-defined drop-down list queston
+            </Form.Label>
+            <Form.Select className="form-select" id="question3">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question4">
+            <Form.Label>Question 4</Form.Label>
+            <textarea
+              className="form-control"
+              placeholder="Longer form answer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            />
+          </Form.Group>{" "}
+          <Form.Group className="mb-3 mt-2" controlId="question1">
+            <Form.Label>Queston 1</Form.Label>
+            <Form.Control type="text" placeholder="Short answer" />
+            <Form.Text className="text-muted">
+              Add additional instructions if needed here.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question2" required>
+            <Form.Label>
+              Question 2 <span className="text-danger fs-4">*</span>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Another short answer" />
+          </Form.Group>
+          <Form.Group className="col-sm-6 mb-3">
+            <Form.Label for="question3">
+              Pre-defined drop-down list queston
+            </Form.Label>
+            <Form.Select className="form-select" id="question3">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question4">
+            <Form.Label>Question 4</Form.Label>
+            <textarea
+              className="form-control"
+              placeholder="Longer form answer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            />
+          </Form.Group>{" "}
+          <Form.Group className="mb-3 mt-2" controlId="question1">
+            <Form.Label>Queston 1</Form.Label>
+            <Form.Control type="text" placeholder="Short answer" />
+            <Form.Text className="text-muted">
+              Add additional instructions if needed here.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question2" required>
+            <Form.Label>
+              Question 2 <span className="text-danger fs-4">*</span>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Another short answer" />
+          </Form.Group>
+          <Form.Group className="col-sm-6 mb-3">
+            <Form.Label for="question3">
+              Pre-defined drop-down list queston
+            </Form.Label>
+            <Form.Select className="form-select" id="question3">
+              <option selected>Choose...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="question4">
+            <Form.Label>Last Question 4</Form.Label>
+            <textarea
+              className="form-control"
+              placeholder="Longer form answer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            />
+          </Form.Group>
+        </Form>
+      </div>
+      <Button
+        type="button"
+        className="btn btn-success"
+        onClick={handleDownload}
+      >
+        💾 📄 Download as a PDF
+      </Button>
+      <Link to="/" role="button" className="m-4 btn btn-warning ">
+        I've downloaded the form
+      </Link>
+    </>
+  );
+}
+
+export default CourtInfo;
