d7631b871ebd3355ecfb3d203ae98d381c8c4df3
tj2904 <tj2904+github@gmail.com>
2022-12-08 17:20:00 +0000

    refactors alert

--- src/pages/HasSolicitor.jsx
+++ src/pages/HasSolicitor.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Alert } from "react-bootstrap";
+import AlertAttendCourt from "../components/AlertAttendCourt";
 
 function HasSolicitor() {
   return (
@@ -15,15 +16,7 @@ function HasSolicitor() {
           help you give them all the details that they need to support you.
         </p>
       </div>
-      <Alert variant="danger" className="mt-4 mx-6 d-flex align-items-center">
-        <div className="flex-shrink-0 me-2 fs-2">⚠️</div>
-        <div>
-          Please note: It is very important that you have your case heard, so
-          <strong> DO NOT MISS YOUR COURT DATE</strong>. Use this website's
-          report to show the Judge/Duty Solicitor at court on the day about your
-          case and have your side of the case put across!
-        </div>
-      </Alert>
+      <AlertAttendCourt />
     </>
   );
 }
