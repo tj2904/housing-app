f0f3feb77fbf07d845907a96f1a7dbcde0f76071
tj2904 <tj2904+github@gmail.com>
2022-12-08 10:36:15 +0000

    implimented changes

--- src/pages/Eligibility.jsx
+++ src/pages/Eligibility.jsx
@@ -1,4 +1,5 @@
 import React  from "react";
+import { Card, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import checkScreen from "../img/legalAidCheckScreen.png";
 import outcomeNo from "../img/outcomeNo.png";
@@ -14,47 +15,63 @@ function Eligibility() {
       </p>
 
       <p>
-        Examples of images they will see, ie. both outcomes and the page the
-        should see first.
-      </p>
-      <p>
         Once you have completed the Government's eligibility check, come back to
-        this page and select the image that matches your outcome. If you may
-        quality for Legal Aid, then we will provide you with a list of
-        Solicitors that maybe able to help you.
+        this page and select the image that matches your outcome. If you see <strong>"Contact Civil Legal Advice"</strong>, you can do that or contact a Solicitor directly for Legal Aid, then we will provide you with a list of
+        Solicitors that might able to help you.
       </p>
       <p>
         If you do not qualify for Legal Aid, then you should take the form you
         have just downloaded with you on your court date.
       </p>
-      <div className="text-center">
-        <a
-          href="https://www.gov.uk/check-legal-aid"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={checkScreen}
-            className="fluid m-2 img-thumbnail"
-            alt="screenshot of the government eligibility check"
-          />
-        </a>
-        <a href="/nosolicitor">
-          <img
-            src={outcomeNo}
-            className="fluid m-2 img-thumbnail"
-            alt="alt text"
-          />
-        </a>
-        <a href="/details">
-          <img
-            src={outcomeYes}
-            className="fluid m-2 img-thumbnail"
-            alt="alt text"
-          />
-        </a>
+      <div className="mx-auto">
+        <div className="text-center mb-4 justify-content-center">
+          <Row xs={1} md={2} className="g-4">
+            <Card className="col-3 p-0">
+              <a href="https://www.gov.uk/check-legal-aid" target="blank">
+                <Card.Body>
+                  <img
+                    src={checkScreen}
+                    className="fluid m-2 img-thumbnail"
+                    alt="screenshot of the government eligibility check"
+                  />
+                </Card.Body>
+                <Card.Footer>Check Eligibity</Card.Footer>
+              </a>
+            </Card>
+          </Row>
+          <Row>
+            <Card className="col-3 p-0">
+              <Link to="/noSolicitor">
+                <Card.Body>
+                  <img
+                    src={outcomeNo}
+                    className="fluid m-2 img-thumbnail"
+                    alt="screenshot of the government eligibility check"
+                  />
+                </Card.Body>
+                <Card.Footer className="link-danger fw-bold">
+                  Not likely to qualify
+                </Card.Footer>
+              </Link>
+            </Card>
+            <Card className="col-3 p-0">
+              <Link to="/details">
+                <Card.Body>
+                  <img
+                    src={outcomeYes}
+                    className="fluid m-2 img-thumbnail"
+                    alt="screenshot of the government eligibility check"
+                  />
+                </Card.Body>
+                <Card.Footer className="link-success fw-bold">
+                  Likely to qualify
+                </Card.Footer>
+              </Link>
+            </Card>
+          </Row>
+        </div>
       </div>
-{/*
+      {/*
       <div>
         <a
           href="https://www.gov.uk/check-legal-aid"
@@ -70,7 +87,7 @@ function Eligibility() {
         </Link>
       </div>
 */}
-</>
+    </>
   );
 }
 
