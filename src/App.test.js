91aacefaeffbc365644dea8edb59c31166bc57d2
tj2904 <tj2904+github@gmail.com>
2022-12-09 01:11:25 +0000

    adds test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../src/components/App'
 
-test('renders learn react link', () => {
+test('renders site title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Eviction Prevention");
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/SolicitorCard.jsx
+++ src/components/SolicitorCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Card } from "react-bootstrap";
-import CardHeader from "react-bootstrap/esm/CardHeader";
 
 export default function  SolicitorCard(firm) {
 
